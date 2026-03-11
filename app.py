3236576734c95bf0cf34fca74cf490fbff943229
Ayush K <77693062+ayusht9@users.noreply.github.com>
2026-03-11 17:12:36 +0530

    Update predictor to include passenger class input

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import joblib
-import numpy as np
 
 model = joblib.load("model.pkl")
 
-st.title("Decision Tree Predictor")
+st.title("Titanic Survival Predictor")
 
+pclass = st.selectbox("Passenger Class", [1,2,3])
 age = st.number_input("Age")
 fare = st.number_input("Fare")
 
 if st.button("Predict"):
 
-    prediction = model.predict([[age, fare]])
-    st.success(f"Prediction: {prediction[0]}")
+    prediction = model.predict([[pclass, age, fare]])
+
+    st.write("Prediction:", prediction)
